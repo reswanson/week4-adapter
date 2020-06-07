d93a1cade6321276c25d23c689b04b99b2d51089
reswanson <swansonr3@csp.edu>
2020-06-06 21:30:08 -0500

    checkpoint

--- src/IImmutableList.java
+++ src/IImmutableList.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 interface IImmutableList {
+	
+	
      public void animalSound(String says); // interface method (does not have a body)
      public void sleep(int[] howlong); // interface method (does not have a body)
      public void eat(String food); // interface method (does not have a body)
@@ -18,24 +20,35 @@ interface IImmutableList {
 	 * @return the value at index location
 	 */
 	 public int get(int index);
+	 
+	 // Test method to try...
+	 public String keeptrack(String mystr);
+	 public String printit();
+
+	 
+	 
 	 /**
 	 * Returns the concatenation of the current list and other list.
 	 * @param list The other list
 	 * @return An immutable list containing elements from both list.
 	 */
-	 
 	 public IImmutableList concat(IImmutableList list);
+	 
+	 
 	 /**
 	 * Returns the number of elements in the list.
 	 * @return number of elements in list.
 	 */
-	 
 	 public int size();
+	 
+	 
+	 
 	 /**
 	 * Return a string presentation of the list.
 	 * The content is enclosed in [ ], Each element is separated by a comma.
 	 * @return string representation of the list.
 	 */
+	 
 	 @Override
 	 public String toString();
 
--- src/ImmutableList.java
+++ src/ImmutableList.java
@@ -1,88 +1,124 @@
 import java.util.ArrayList;
 
-
-
 public class ImmutableList implements IImmutableList {
-      
-	  private String tripName;
-	  ArrayList<String> locations;
-	  
-	  public void Trip(String tripName) {
+
+	// ----------------------
+
+	private String tripName;
+	ArrayList<String> locations;
+	// ArrayList<Int> stuff;
+
+	String myinternalstring = "";
+
+	
+	
+	// method  I just created to start playing with concatting two strings.	
+	public String keeptrack(String mystr) {
+		// This is the string I want to keep adding things to.
+		this.myinternalstring = this.myinternalstring + mystr;
+		System.out.println("in keeptrack: " + this.myinternalstring);
+		return (this.myinternalstring);
+	}
+
+	public String printit() {
+		return (this.myinternalstring);
+	}
+
+	
+	
+	
+	
+	// Your methods from Trip
+	public void Trip(String tripName) {
 		this.tripName = tripName;
 		locations = new ArrayList<String>();
-	  }
-	  
-	  public void addLocation(String location) {
-		    System.out.println("Adding location" + location);
-	        locations.add(location);
-	  }
-	  
-	  //Getters and Setters
-	  public String getTripName() {
-	    	return tripName;
-	  }
+	}
 
-	  public void setTripName(String tripName) {
-	    	this.tripName = tripName;
-	  }
+	public void addLocation(String location) {
+		System.out.println("Adding location" + location);
+		locations.add(location);
+	}
 
-	  // ----------------------
-	  
-	  public void animalSound(String says) {
-	    // The body of animalSound() is provided here
-	    System.out.println("The pig says in ImmutableList: " + says);
-	  }
-	  
-	  public void eat(String food) {
-		    // The body of eat() is provided here
-		    System.out.println("im hungry, im gonna eat: " + food);
-	  }
-	  
-	  public void sleep(int[] howlong) {
-		    // The body of sleep() is provided here
-		    // just picking the first element of array for fun to make sure I can pass in array 
-		    System.out.println("taking a long snooze: " + howlong[0]);
-	  }
-	  
-	  
-	 /**
-	  * Returns the element at position index
-	  * @param index the index position of the list
-	  * @return the value at index location
-	  */
-	 public int get(int index) { 
-		    System.out.println("taking a long snooze: " );
-		    return(0);
-	 }
-	 
-	 /**
+	// Getters and Setters
+	public String getTripName() {
+		return tripName;
+	}
+
+	public void setTripName(String tripName) {
+		this.tripName = tripName;
+	}
+
+	
+	
+	// Samples from W3schools for an interface.
+	// ----------------------
+
+	public void animalSound(String says) {
+		System.out.println("The pig says in ImmutableList: " + says);
+	}
+
+	public void eat(String food) {
+		System.out.println("im hungry, im gonna eat: " + food);
+	}
+
+	public void sleep(int[] howlong) {
+		// just picking the first element of array for fun to make sure I can pass in
+		// array
+		System.out.println("taking a long snooze: " + howlong[0]);
+	}
+
+	
+	
+	// For our assignment.
+	// -----------------------------------
+	/**
+	 * Returns the element at position index
+	 * 
+	 * @param index the index position of the list
+	 * @return the value at index location
+	 */
+	public int get(int index) {
+		return (0);
+	}
+
+	/**
 	 * Returns the concatenation of the current list and other list.
+	 * 
 	 * @param list The other list
 	 * @return An immutable list containing elements from both list.
 	 */
-	 public ArrayList<String> concat(IImmutableList list) {
-	 }
-	 
-	 
-	 /**
+	// public IImmutableList concat(IImmutableList list) {
+	// Took the code from the keeptrack method above, baby steps to concat string
+
+	// this.myinternalstring = this.myinternalstring + mystr;
+	// System.out.println("in concat" + this.myinternalstring);
+	// return(this.myinternalstring);
+	// }
+
+	/**
 	 * Returns the number of elements in the list.
+	 * 
 	 * @return number of elements in list.
 	 */
-	 public int size() {
-		 return(0);
-	 }
-	 
-	 
-	 
-	 /**
-	 * Return a string presentation of the list.
-	 * The content is enclosed in [ ], Each element is separated by a comma.
+	public int size() {
+		return (0);
+	}
+
+	/**
+	 * Return a string presentation of the list. The content is enclosed in [ ],
+	 * Each element is separated by a comma.
+	 * 
 	 * @return string representation of the list.
 	 */
-	 @Override
-	 public String toString() {
-		 return("123");
-	 }
+	@Override
+	public String toString() {
+		// for (ArrayList<Int> t : this.stuff) {
+		// // Build string to return
+		// return()
+		// System.out.println(t.getTripName() + " - " + t.getLocations().size());
+		//
+		// }
+		return (this.myinternalstring);
+	}
 
 }
-
